6b3cb08d44a2822176208489ec926ebd01685996
zamasu <zxm273293@gmail.com>
2025-12-15 16:12:08 +0300

    Create PLAY_ACTIVITY.js

---
+++ src/quests/PLAY_ACTIVITY.js
@@ -0,0 +1,44 @@
+import QuestConfig from "../lib/questConfig.js";
+import ms from "ms";
+
+export default new QuestConfig({
+    name: "PLAY_ACTIVITY",
+    requireLogin: false,
+    requireVoiceChannel: false,
+    
+    async run(user) {
+        const secondsNeeded = user.target;
+        let progress = user.current || 0;
+        const streamKey = `call:${user.id}:1`;
+        
+        while (!user.stoped) {
+            try {
+                const heartbeat = await user.api.post(`quests/${user.quest}/heartbeat`, {
+                    stream_key: streamKey,
+                    terminal: false
+                });
+                
+                if (!heartbeat?.data?.user_id) {
+                    user.stop("Error sending heartbeat");
+                    break;
+                }
+                
+                const response = user.extractProgress(heartbeat.data);
+                progress = response.value;
+                user.sendUpdate(progress, response.completed);
+                
+                if (progress >= secondsNeeded || response.completed) {
+                    user.stop();
+                    user.completed = true;
+                    break;
+                }
+                
+                await user.delay(ms("30s"));
+            } catch (error) {
+                console.error("Error in PLAY_ACTIVITY quest:", error);
+                user.stop("Heartbeat request failed");
+                break;
+            }
+        }
+    }
+});
